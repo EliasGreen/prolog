17044a5006082d1b13d7ec1b9dcfd1f13927675a
EliasGreen <akcuoma@rambler.ru>
2020-04-08 12:43:48 +0300

    sad story

---
+++ Inds prolog (12 var)/Ind 6/first attempt.pl
@@ -0,0 +1,16 @@
+node(a,graph).
+node(b,graph).
+node(c,graph).
+node(d,graph).
+
+edge(a,b,graph).
+edge(c,b,graph).
+edge(d,c,graph).
+
+notCycled(graph)-:.
+
+connected(graph):-.
+
+tree(graph):-
+	notCycled(graph),
+	connected(graph).
